71f67f99f910d494c191e16b5d310041e987c5c7
Gintare Venckiene <gintare.venckiene@gmail.com>
2020-12-10 21:22:21 +0200

    Added some new tests

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,4 +1,21 @@
-it('Shows an autocomplete', () => {
+const waitFor = (selector) => {
+	return new Promise((resolve, reject) => {
+		const interval = setInterval(() => {
+			if(document.querySelector(selector)){
+				clearInterval(timeout);
+				resolve();
+			}
+		}, 30);
+
+		const timeout = setTimeout(() => {
+			clearInterval(interval)
+			reject();
+		}, 2000)
+	});
+};
+
+beforeEach(() => {
+	document.querySelector('#target').innerHTML = '';
 	createAutoComplete({
 		root: document.querySelector('#target'),
 		fetchData() {
@@ -15,9 +32,26 @@ it('Shows an autocomplete', () => {
 			return movie.Title;
 		}
 	});
+});
 
+
+it('Dropdown starts closed', () => {
 	const dropdown = document.querySelector('.dropdown');
 
+	expect(dropdown.className).not.to.include('is-active');
+
+});
+
+it('After search, dropdown shows up', async () => {
 	
+	const input = document.querySelector('input');
+	input.value = "search term";
+
+	input.dispatchEvent(new Event('input'));
+
+	await waitFor('.dropdown-item');
+	
+	const dropdown = document.querySelector('.dropdown');
+	expect(dropdown.className).to.include('is-active');
 });
 
